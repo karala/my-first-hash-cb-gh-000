569b8e969e8a0ffa3e1437837d52943ae3080af1
Shubham Karala <shubham@ramanujan.du.ac.in>
2019-10-29 00:44:59 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -13,9 +13,10 @@ def shipping_manifest
   # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings
   the_manifest = {
     "whale bone corsets" => 5,
-    "porcelain vases" => 2,
+    "porcelainvases" => 2,
     "oil paintings" => 3
   }
+  the_manifest[oil paintings]
 
 end
 
